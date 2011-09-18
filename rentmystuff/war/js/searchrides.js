2ae7f00e3426d1aa1fb55b1f8477d64ce5abe5fe
paritosh <paritosh@paritosh-PC>
2011-09-18 13:25:11 +0530

    working copy

---
+++ rentmystuff/war/js/searchrides.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+function initialize() {
+	alert("inside initialize ");
+	var input_from = document.getElementById('fromSearch');
+	var input_to = document.getElementById('toSearch');
+	var autocomplete_from = new google.maps.places.Autocomplete(input_from);
+	var autocomplete_to = new google.maps.places.Autocomplete(input_to);
+
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		var place_from = autocomplete_from.getPlace();
+		var place_to = autocomplete_to.getPlace();
+	});
+}
+//google.maps.event.addDomListener(window, 'load', initialize);
