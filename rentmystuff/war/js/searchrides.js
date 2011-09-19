a77a327860b3d71c9636ec544b369e4852031f53
Gautam <Gautam@gautamlaptop>
2011-09-20 02:06:30 +0530

    blah

--- rentmystuff/war/js/searchrides.js
+++ rentmystuff/war/js/searchrides.js
@@ -3,10 +3,14 @@
  */
 
 function initialize() {
-	var input_from = document.getElementById('fromSearch');
-	var input_to = document.getElementById('toSearch');
+	var input_from = document.getElementById('fromFind');
+	var input_to = document.getElementById('toFind');
+	alert('here:'+input_to);
+	input_to.value = 'setvalue';
+	alert('after setting value:'+input_to);
 	var autocomplete_from = new google.maps.places.Autocomplete(input_from);
 	var autocomplete_to = new google.maps.places.Autocomplete(input_to);
+	alert('here 2');
 	google.maps.event.addListener(autocomplete_from, 'place_changed', function() {
 		var place_from = autocomplete_from.getPlace();
 	});
