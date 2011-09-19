6cabbeca098c3fb359108b889944bfa91a2fda44
paritosh <paritosh@paritosh-PC>
2011-09-20 01:33:30 +0530

    ride.html + search_rides.html fixed!!

--- rentmystuff/war/js/searchrides.js
+++ rentmystuff/war/js/searchrides.js
@@ -2,18 +2,23 @@
  * 
  */
 
-function initialize() {
-	var input_from = document.getElementById('fromSearch');
-	var input_to = document.getElementById('toSearch');
-	var autocomplete_from = new google.maps.places.Autocomplete(input_from);
-	var autocomplete_to = new google.maps.places.Autocomplete(input_to);
+function initialize(fromText, toText ) {
+	var from_AddRide = document.getElementById(fromText);
+	var to_AddRide = document.getElementById(toText);
+	var autocomplete_from = new google.maps.places.Autocomplete(from_AddRide);
+	var autocomplete_to = new google.maps.places.Autocomplete(to_AddRide);
+	
+
 	google.maps.event.addListener(autocomplete_from, 'place_changed', function() {
 		var place_from = autocomplete_from.getPlace();
 	});
 	google.maps.event.addListener(autocomplete_to, 'place_changed', function() {
 		var place_to = autocomplete_to.getPlace();
 	});
+
 }
+
+
 //google.maps.event.addDomListener(window, 'load', initialize);
 
 
