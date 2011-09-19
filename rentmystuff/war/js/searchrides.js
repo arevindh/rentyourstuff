12157a0de38aec5cb1aafa4ea996e27d2a56550b
paritosh <paritosh@paritosh-PC>
2011-09-19 21:31:27 +0530

    1. content loader from folder <content> inside <pages> #please fix: shows not found error
    2. adding buttons to rides page

--- rentmystuff/war/js/searchrides.js
+++ rentmystuff/war/js/searchrides.js
@@ -17,18 +17,3 @@ function initialize() {
 
 //TEST!!
 
-function showRequest(formData, jqForm, options) { 
-    // formData is an array; here we use $.param to convert it to a string to display it 
-    // but the form plugin does this for you automatically when it submits the data 
-    var queryString = $.param(formData); 
- 
-    // jqForm is a jQuery object encapsulating the form element.  To access the 
-    // DOM element for the form do this: 
-    // var formElement = jqForm[0]; 
- 
-    alert('About to submit: \n\n' + queryString); 
- 
-    // here we could return false to prevent the form from being submitted; 
-    // returning anything other than false will allow the form submit to continue 
-    return true; 
-} 
